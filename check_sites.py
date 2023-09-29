6d5a697e70630cc10ff80ff2b7cb06a2322a40ca
Anupam Singh <64646798+AnupamAS0x1@users.noreply.github.com>
2023-09-29 11:00:19 +0530

    Update check_sites.py

--- check_sites.py
+++ check_sites.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-print("anupamas01") 
 from FpsCheck import FpsCheck
 import json
 import getopt
